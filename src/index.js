d07c56c31a987615625465a90b9b495b37694059
Gean Ferreira <gean_roberto@hotmail.com>
2022-01-19 22:11:29 -0300

    creats a new folder for the main content - calculator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-import Calculator from './Components/Calculator';
+import Calculator from './main-content/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
--- src/main-content/Calculator.jsx
+++ src/main-content/Calculator.jsx
@@ -1,4 +1,5 @@
 import '../style/Calculator.css'
+import Button from '../Components/Buttons'
 
 const Calculator = () => {
   return(
