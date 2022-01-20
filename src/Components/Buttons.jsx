730d671d47708962e2012f6a293812ab3eb4bb75
Gean Ferreira <gean_roberto@hotmail.com>
2022-01-19 22:06:21 -0300

    creats a Button Component

---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,20 @@
+export default props => {
+  
+  let classes = 'button '
+  // increment in className if the button has 
+  // operation
+  // in your proprities
+  classes += props.operation ? 'operation' : '' 
+  // increment in className if the button has 
+  // twoCol
+  // in your proprities
+  classes += props.twoCol ? 'twoCol' : ''
+  // increment in className if the button has 
+  // threeCol
+  // in your proprities
+  classes += props.ThreeCol ? 'ThreeCol' : ''
+
+  return(
+    <button className={classes}>{props.value}</button>
+  )
+}
