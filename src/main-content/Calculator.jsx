6f06a5214c24c55d279592619ac7d5540d54a561
Gean Ferreira <gean_roberto@hotmail.com>
2022-01-22 07:43:26 -0300

    puts buttons in the calculator component and their values

--- src/main-content/Calculator.jsx
+++ src/main-content/Calculator.jsx
@@ -5,7 +5,25 @@ const Calculator = () => {
   return(
     <>
       <h1>Calculator</h1>
-      <div className="calculator"></div>
+      <div className="calculator">
+        <Button value={"CLEAR ALL"}/>
+        <Button value={"/"}/>
+        <Button value={7}/>
+        <Button value={8}/>
+        <Button value={9}/>
+        <Button value={"x"}/>
+        <Button value={4}/>
+        <Button value={5}/>
+        <Button value={6}/>
+        <Button value={"-"}/>
+        <Button value={1}/>
+        <Button value={2}/>
+        <Button value={3}/>
+        <Button value={"+"}/>
+        <Button value={0}/>
+        <Button value={"."}/>
+        <Button value={"="}/>
+      </div>
     </>
   )
 }
