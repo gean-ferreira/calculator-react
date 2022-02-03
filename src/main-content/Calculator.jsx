10a96c41c6e97ea5d079730cd282e696021848a0
Gean Ferreira <gean_roberto@hotmail.com>
2022-02-02 21:26:37 -0300

    Set the variables to be tracking and add the event to the number buttons

--- src/main-content/Calculator.jsx
+++ src/main-content/Calculator.jsx
@@ -1,29 +1,37 @@
 import '../style/Calculator.css'
 import Button from '../Components/Buttons'
 import Display from '../Components/Display'
+import { useState } from 'react'
 
 const Calculator = () => {
+
+  const [calc, setCalc] = useState("");
+
+  const updateCalc = value => {
+    setCalc(calc + value)
+  }
+
   return(
     <>
       <h1>Calculator</h1>
       <div className="calculator">
-        <Display/>
+        <Display value={calc || 0}/>
         <Button value={"CLEAR ALL"} threeCol/>
         <Button value={"/"} operation/>
-        <Button value={7}/>
-        <Button value={8}/>
-        <Button value={9}/>
+        <Button value={7} click={(e) => updateCalc(e)} />
+        <Button value={8} click={(e) => updateCalc(e)} />
+        <Button value={9} click={(e) => updateCalc(e)} />
         <Button value={"x"} operation/>
-        <Button value={4}/>
-        <Button value={5}/>
-        <Button value={6}/>
+        <Button value={4} click={(e) => updateCalc(e)} />
+        <Button value={5} click={(e) => updateCalc(e)} />
+        <Button value={6} click={(e) => updateCalc(e)} />
         <Button value={"-"} operation/>
-        <Button value={1}/>
-        <Button value={2}/>
-        <Button value={3}/>
+        <Button value={1} click={(e) => updateCalc(e)} />
+        <Button value={2} click={(e) => updateCalc(e)} />
+        <Button value={3} click={(e) => updateCalc(e)} />
         <Button value={"+"} operation/>
-        <Button value={0} twoCol/>
-        <Button value={"."}/>
+        <Button value={0} click={(e) => updateCalc(e)} twoCol/>
+        <Button value={"."} click={(e) => updateCalc(e)} />
         <Button value={"="} operation/>
       </div>
     </>
