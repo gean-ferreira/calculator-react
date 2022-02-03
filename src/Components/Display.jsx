df3ccbab06afdacd0d9a9633b393d91fdca537d4
Gean Ferreira <gean_roberto@hotmail.com>
2022-02-02 21:09:36 -0300

    Add props to the Display Component

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,11 +1,11 @@
 import '../style/ComponentsCss/Display.css'
 
-const Display = () => {
+const Display = props => {
   return(
     <>
       <div className="display">
         <div className="expression">12 + 12 = 24</div>
-        <div className="inputValues">999</div>
+        <div className="inputValues">{props.value}</div>
       </div>
     </>
   )
