04329d011f1e73767bca2875efcabec84f95d17f
Gean Ferreira <gean_roberto@hotmail.com>
2022-02-08 20:58:40 -0300

    Format the doc and add the functions name

--- src/main-content/Calculator.jsx
+++ src/main-content/Calculator.jsx
@@ -1,59 +1,59 @@
-import '../style/Calculator.css'
-import Button from '../Components/Buttons'
-import Display from '../Components/Display'
-import { useState } from 'react'
+import "../style/Calculator.css";
+import Button from "../Components/Buttons";
+import Display from "../Components/Display";
+import { useState } from "react";
 
 const Calculator = () => {
-
   const [calc, setCalc] = useState("");
 
-  const oprt = ['/', '*', '+', '-']
+	const oprt = ["/", "*", "+", "-", "."];
 
-  const updateCalc = value => {
+	const updateCalc = (value) => {
     // if the display has no number doesnt include an operator 
     // or
     // has an operator in it (on the display) cannot put any operator
-    if(calc === '' && oprt.includes(value) || oprt.includes(value) && oprt.includes(calc.slice(-1))) {
+		if ((calc === "" && oprt.includes(value)) || (oprt.includes(value) && oprt.includes(calc.slice(-1)))) {
       return;
     }
-    setCalc(calc + value)
-  }
+
+		setCalc(calc + value);
+	};
 
   return(
     <>
       <h1>Calculator</h1>
       <div className="calculator">
-        <Display value={calc || 0}/>
-        <Button value={"CLEAR ALL"} threeCol/>
+				<Display value={calc || 0} />
+				<Button value={"CLEAR ALL"} click={clearAll} threeCol />
 
-        <Button value={"/"} click={(e) => updateCalc(e)} operation/>
+				<Button value={"/"} click={(e) => updateCalc(e)} operation />
 
         <Button value={7} click={(e) => updateCalc(e)} />
         <Button value={8} click={(e) => updateCalc(e)} />
         <Button value={9} click={(e) => updateCalc(e)} />
 
-        <Button value={"*"} click={(e) => updateCalc(e)} operation/>
+				<Button value={"*"} click={(e) => updateCalc(e)} operation />
 
         <Button value={4} click={(e) => updateCalc(e)} />
         <Button value={5} click={(e) => updateCalc(e)} />
         <Button value={6} click={(e) => updateCalc(e)} />
 
-        <Button value={"-"} click={(e) => updateCalc(e)} operation/>
+				<Button value={"-"} click={(e) => updateCalc(e)} operation />
 
         <Button value={1} click={(e) => updateCalc(e)} />
         <Button value={2} click={(e) => updateCalc(e)} />
         <Button value={3} click={(e) => updateCalc(e)} />
 
-        <Button value={"+"} click={(e) => updateCalc(e)} operation/>
+				<Button value={"+"} click={(e) => updateCalc(e)} operation />
 
-        <Button value={0} click={(e) => updateCalc(e)} twoCol/>
+				<Button value={0} click={(e) => updateCalc(e)} twoCol />
 
         <Button value={"."} click={(e) => updateCalc(e)} click={(e) => updateCalc(e)} />
 
-        <Button value={"="} click={(e) => updateCalc(e)} operation/>
+				<Button value={"="} click={calculate} operation />
       </div>
     </>
-  )
-}
+	);
+};
 
 export default Calculator;
