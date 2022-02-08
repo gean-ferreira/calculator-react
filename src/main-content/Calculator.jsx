fb71a7827193fc8f6626252eb4e0e8314b130113
Gean Ferreira <gean_roberto@hotmail.com>
2022-02-07 22:01:21 -0300

    separates operators and includes logic in them

--- src/main-content/Calculator.jsx
+++ src/main-content/Calculator.jsx
@@ -17,22 +17,32 @@ const Calculator = () => {
       <div className="calculator">
         <Display value={calc || 0}/>
         <Button value={"CLEAR ALL"} threeCol/>
-        <Button value={"/"} operation/>
+
+        <Button value={"/"} click={(e) => updateCalc(e)} operation/>
+
         <Button value={7} click={(e) => updateCalc(e)} />
         <Button value={8} click={(e) => updateCalc(e)} />
         <Button value={9} click={(e) => updateCalc(e)} />
-        <Button value={"x"} operation/>
+
+        <Button value={"*"} click={(e) => updateCalc(e)} operation/>
+
         <Button value={4} click={(e) => updateCalc(e)} />
         <Button value={5} click={(e) => updateCalc(e)} />
         <Button value={6} click={(e) => updateCalc(e)} />
-        <Button value={"-"} operation/>
+
+        <Button value={"-"} click={(e) => updateCalc(e)} operation/>
+
         <Button value={1} click={(e) => updateCalc(e)} />
         <Button value={2} click={(e) => updateCalc(e)} />
         <Button value={3} click={(e) => updateCalc(e)} />
-        <Button value={"+"} operation/>
+
+        <Button value={"+"} click={(e) => updateCalc(e)} operation/>
+
         <Button value={0} click={(e) => updateCalc(e)} twoCol/>
-        <Button value={"."} click={(e) => updateCalc(e)} />
-        <Button value={"="} operation/>
+
+        <Button value={"."} click={(e) => updateCalc(e)} click={(e) => updateCalc(e)} />
+
+        <Button value={"="} click={(e) => updateCalc(e)} operation/>
       </div>
     </>
   )
